58f440c060bbd844f6719a96b5354b90912f16e8
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-09-27 14:20:10 -0500

    --- Auto-Git Commit ---

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,27 @@
-import {React, useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Post.css'
 import Avatar from '@material-ui/core/Avatar'
+import { db } from '../firebase';
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, username, caption, imageUrl }) {
     const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        let unsubscribe;
+        if (postId) {
+            unsubscribe = db
+                .collection('posts')
+                .doc(postId)
+                .collection('comments')
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map((doc) => doc.data()))
+                });
+        }
+
+        return () => {
+            unsubscribe();
+        }
+    }, [postId]);
 
     return (
         <div className="post">
@@ -21,6 +39,17 @@ function Post({ username, caption, imageUrl }) {
             alt="user" 
             />
             <h4 className="postText"><strong>{username}</strong> {caption}</h4>
+            
+            <form>
+                <input
+                 className="post-input"
+                 type="text"
+                 placeholder="Add a comment..."
+                 value={comments}
+                 onChange={(e) => setComments(e.target.value)}
+                />
+            </form>
+
         </div>
     )
 }
